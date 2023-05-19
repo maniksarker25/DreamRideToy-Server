fc7f99db0abe98ef82c5baa8f2e9808ac60ec9be
Manik <tonysins43@gmail.com>
2023-05-19 16:01:50 +0600

    get data by search by name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,6 +36,22 @@ async function run() {
     app.get('/allToys', async(req,res)=>{
         const result = await toyCollection.find().toArray();
         res.send(result)
+    })
+    
+    app.get('/allToys/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await toyCollection.findOne(query);
+      res.send(result)
+    })
+    app.get('/toy/:name', async(req,res)=>{
+      const toyName = req.params.name;
+      const result = await toyCollection.find({
+        $or:[
+          { toyName: { $regex: toyName, $options: "i" } }
+        ]
+      }).toArray();
+      res.send(result)
     })
 
     app.post('/addToy', async(req,res)=>{
