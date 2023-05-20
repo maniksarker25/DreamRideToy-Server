689a6c330b7e0ee672d136b8bcfbf03267f6387d
Manik <tonysins43@gmail.com>
2023-05-20 22:45:38 +0600

    some changes

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const toyCollection = client.db('toyDB').collection('toys')
 
